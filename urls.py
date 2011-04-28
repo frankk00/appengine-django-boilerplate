5e483b8a581807dfd6ee96f3de4e7527154b9b84
iynaix <iynaix@gmail.com>
2011-04-28 17:22:16 +0800

    Added favicon, robots.txt, humans.txt and sitemap.xml redirects to urls.py.

--- urls.py
+++ urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic import RedirectView, TemplateView, ListView
 
 handler500 = 'djangotoolbox.errorviews.server_error'
 
@@ -6,4 +7,11 @@ urlpatterns = patterns('',
     ('^_ah/warmup$', 'djangoappengine.views.warmup'),
     ('^$', 'django.views.generic.simple.direct_to_template',
      {'template': 'home.html.jin'}),
+)
+
+urlpatterns += patterns("",
+    (r'^favicon\.ico$', RedirectView.as_view(url='/static/img/favicon.ico')),
+    (r'^sitemap\.xml$', RedirectView.as_view(url='sitemap.txt')),
+    (r'^humans\.txt$', TemplateView.as_view(template_name='humans.txt')),
+    (r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt')),
 )
